9fe7b39fedc76279d3b4f38159b48086f4ab6159
rock's ezweb <alikerock@gmail.com>
2024-11-08 15:09:45 +0900

    로그인 완료

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -7,6 +7,7 @@ const Auth = ()=>{
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [newAccount, setNewAccount] = useState(true);
+  const [error, setError] = useState('');
 
   const auth = getAuth();
 
@@ -37,18 +38,32 @@ const Auth = ()=>{
       //회원가입
       createUserWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
-        //계정생성완료후 할일  
+        //계정생성완료후, 로그인완료   
         const user = userCredential.user; //생성된 계정의 유저정보 확인
         console.log(user);
       })
       .catch((error) => {
-        const errorCode = error.code;
+        //const errorCode = error.code;
         const errorMessage = error.message;  
-        console.log(errorCode, errorMessage)      
+        setError(errorMessage);
       });
     } else{
       //로그인
+      signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        //const errorCode = error.code;
+        const errorMessage = error.message;
+        setError(errorMessage);
+      });
     }
+  }
+  const toggleAccount = ()=>{
+    setNewAccount(prev=>!prev);
   }
   return(
     <div className="container">
@@ -61,9 +76,12 @@ const Auth = ()=>{
         <Form.Group className="mb-3" controlId="loginPW">
           <Form.Label>Password</Form.Label>
           <Form.Control type="password" name="password" onChange={onChange} />
-        </Form.Group>
-        <Button variant="primary" type="submit">Login</Button>
+        </Form.Group>        
+        <Button variant="primary" type="submit">{newAccount? '회원가입' : '로그인'}</Button>
+        <div>{error}</div>
       </Form>
+      <hr/>
+      <Button variant="secondary" onClick={toggleAccount}>{newAccount? '로그인으로 전환' : '회원가입으로 전환'}</Button>
     </div>
   )
 }
