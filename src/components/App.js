a1301a62f32498f4dee97231a84d20dc1fb2b1fc
rock's ezweb <alikerock@gmail.com>
2024-11-08 12:20:25 +0900

    AuthService 추가

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,14 @@
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import AppRouter from './Router';
+import { authService } from '../firebase';
+console.log(authService);
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
-    <AppRouter/>
+    <AppRouter isLoggedIn={isLoggedIn}/>
   );
 }
 
