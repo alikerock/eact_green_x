ab1d46b99dcee2652b110ee002cb2510109b9b9c
rock's ezweb <alikerock@gmail.com>
2024-11-11 13:07:19 +0900

    글 수정 삭제

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,27 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ListGroup from 'react-bootstrap/ListGroup';
 import { Button } from 'react-bootstrap';
-import { doc, deleteDoc } from "firebase/firestore";
+import { doc, deleteDoc, updateDoc} from "firebase/firestore";
+import { db } from '../firebase';
+import Form from 'react-bootstrap/Form';
 
 const Comment = ({commentObj, isOwener})=>{
+  const [edit, setEdit] = useState(false);
+  const [comment, setComment] = useState(commentObj.comment); //이전 글을 초기값
 
-  const deleteComment = ()=>{
+  const deleteComment = async ()=>{ 
     const deleteConfirm = window.confirm('정말 삭제할까요?');
     if(deleteConfirm){
-      
+      await deleteDoc(doc(db, "comments", commentObj.id));
     }
+  }
+  const toggleEditMode = ()=>{
+    setEdit(prev=>!prev);
+  }
+  
+  const onChange = (e)=>{
+    // let value = e.target.value;
+    const {target:{value}} = e;
+    setComment(value);
+  }
+  const onSubmit = async (e)=>{
+    e.preventDefault();
+    const commentRef = doc(db, "comments", commentObj.id);
+    await updateDoc(commentRef, {
+      comment: comment
+    });
+    setEdit(false);
   }
 
   return(
     <ListGroup.Item>
       <div className='d-flex justify-content-between'>
-        {commentObj.comment}
-        {isOwener &&            
-          <div className='d-flex gap-1'>
-            <Button variant="secondary"  size="sm">수정</Button>
-            <Button variant="danger" onClick={deleteComment}  size="sm">삭제</Button>
-          </div> 
+        {edit ? 
+          <Form onSubmit={onSubmit}>
+            <Form.Group className="mb-3" controlId="comment">          
+              <Form.Control type="text" value={comment} onChange={onChange} placeholder="글을 입력해주세요" />
+            </Form.Group>
+            <Button variant="info" type="button" onClick={toggleEditMode}>취소</Button>
+            <Button variant="success" type="submit">입력</Button>
+          </Form>
+        : 
+        (
+          <>
+            {commentObj.comment}
+            {isOwener &&            
+              <div className='d-flex gap-1'>
+                <Button variant="secondary" onClick={toggleEditMode}  size="sm">수정</Button>
+                <Button variant="danger" onClick={deleteComment}  size="sm">삭제</Button>
+              </div> 
+            }
+          </>
+          )   
         }
+
    
       </div>
     </ListGroup.Item> 
