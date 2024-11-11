d1b74754226953d2a63bafc1127722449edfe6de
rock's ezweb <alikerock@gmail.com>
2024-11-11 11:02:15 +0900

    글 목록 조회 출력

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,12 +1,40 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import { Button } from 'react-bootstrap';
 import {db} from "../firebase";
-import { collection, addDoc, serverTimestamp  } from "firebase/firestore"; 
+import { collection, addDoc, serverTimestamp, query, where, getDocs } from "firebase/firestore"; 
+import ListGroup from 'react-bootstrap/ListGroup';
 
 const Home = ()=>{
-  const [comment, setComment] = useState('');
+  const [comment, setComment] = useState(''); //입력하는 글 정보
+  const [comments, setComments] = useState([]); //조회된 글 배열
   
+  const getComments = async ()=>{
+    const q = query(collection(db, "comments"));
+
+    const querySnapshot = await getDocs(q);
+    /*
+    querySnapshot.forEach((doc) => {
+      // doc.data() is never undefined for query doc snapshots
+      const commentObj = {
+        ...doc.data(),
+        id:doc.id
+      }
+      setComments(prev=>[commentObj, ...prev]);
+    });
+    */
+
+   //const commentArr = querySnapshot.docs.map(doc=>{return {...doc.data(), id:doc.id}})
+   const commentArr = querySnapshot.docs.map(doc=>({...doc.data(), id:doc.id}))
+   setComments(commentArr);
+
+  }
+  console.log(comments);
+
+  useEffect(()=>{
+    getComments();
+  },[]) //최소 렌더링후 실행, 변동시 실행
+
   const onChange = (e)=>{
     // let value = e.target.value;
     const {target:{value}} = e;
@@ -35,6 +63,12 @@ const Home = ()=>{
         </Form.Group>
         <Button variant="primary" type="submit">입력</Button>
       </Form>
+      <hr/>
+      <ListGroup>
+        {comments.map(item=> 
+          <ListGroup.Item>{item.comment}</ListGroup.Item> 
+        )}        
+      </ListGroup>
     </div>
   )
 }
