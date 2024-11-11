2788b1f2519ab82d1a1a3f551f4c63028cbe7c06
rock's ezweb <alikerock@gmail.com>
2024-11-11 09:46:18 +0900

    comment 입력폼

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import { Button } from 'react-bootstrap';
 
 const Home = ()=>{
+  const [comment, setComment] = useState('');
+  
+  const onChange = (e)=>{
+    // let value = e.target.value;
+    const {target:{value}} = e;
+    setComment(value);
+  }
+  const onSubmit = (e)=>{
+    e.preventDefault();
+  }
+
   return(
     <div className="container">
-      <Form>
+      <Form onSubmit={onSubmit}>
         <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">          
-          <Form.Control type="email" placeholder="글을 입력해주세요" />
+          <Form.Control type="email" onChange={onChange} placeholder="글을 입력해주세요" />
         </Form.Group>
         <Button variant="primary">입력</Button>
       </Form>
