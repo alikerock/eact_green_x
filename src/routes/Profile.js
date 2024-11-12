ec80812ac6fc2964160e94f2b72fbcd395f74b8c
rock's ezweb <alikerock@gmail.com>
2024-11-12 11:14:04 +0900

    profile 이미지 변경

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,9 +1,17 @@
-import React from 'react';
-import { getAuth, signOut } from "firebase/auth";
+import React, { useEffect, useState } from 'react';
+import { getAuth, signOut, updateProfile} from "firebase/auth";
 import Button from 'react-bootstrap/Button';
 import { useNavigate } from 'react-router-dom';
+import { getStorage, ref, uploadBytes, getDownloadURL  } from "firebase/storage";
+import { authService } from '../firebase';
+
+console.log(authService);
 
 const Profile = ()=>{
+  const user = authService.currentUser;
+  const profilePath = `${process.env.PUBLIC_URL}/profile_icon.svg`;
+  const [profile, setProfile] = useState(profilePath);
+
   const navigate = useNavigate();
   const onLogoutClick = () =>{
     const auth = getAuth();
@@ -17,14 +25,31 @@ const Profile = ()=>{
       alert(error);
     });
   }
+  const updateLogo = async (e)=>{
+    const file = e.target.files[0];
+    const storage = getStorage(); //파일업로드 관련 함수 초기화
+    const storageRef = ref(storage, `profile/${user.uid}`); //파일이 저장될위치, 이름지정 
+    const result = await uploadBytes(storageRef, file); //업로드후 결과를 result에 할당
+    const profileURL = await getDownloadURL(result.ref); //업로드된 이미지의 절대 경로
+    setProfile(profileURL); //새이미지로 프로필 교체
+
+    const auth = getAuth();
+    await updateProfile(auth.currentUser, {
+      photoURL: profileURL
+    });
+  }
+  useEffect(()=>{
+    user.photoURL !== null && setProfile(user.photoURL);
+  },[]) //최초한번 실행, 값이 변경되면 실행
+
   return(
     <div className='container'>
       <h1>Profile Page</h1>
       <div className="profile">
-        <img src={`${process.env.PUBLIC_URL}/profile_icon.svg`} alt=""/>
+        <img src={profile} alt=""/>
       </div>           
-        <input type="file" className="hidden" accept="image/*" name="profile" id="profile" />    
-        <Button variant="primary" type="button">프로필 이미지 변경</Button>      
+        <input type="file" className="hidden" accept="image/*" name="profile" id="profile"  onChange={updateLogo}/>    
+        <label className='btn btn-primary' htmlFor="profile">프로필 이미지 변경</label>
       <hr/>      
       <Button variant="primary" onClick={onLogoutClick}>로그아웃</Button>
     </div>
